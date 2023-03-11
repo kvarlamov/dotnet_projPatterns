49d20157fc6daa619969cd869620d29fbd0e1575
Konstantin.Varlamov <k.varlamov88@yandex.ru>
2023-03-11 09:30:58 +0300

    configured program

--- WebApiProj/src/services/WebApiEx/Program.cs
+++ WebApiProj/src/services/WebApiEx/Program.cs
@@ -1,4 +1,8 @@
+using System;
+using System.IO;
+using BaseTypes.Constants;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace WebApiEx
@@ -12,8 +16,25 @@ namespace WebApiEx
             host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(BaseConstants.AppSettingsFileName, false)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            Uri selfUri = new Uri(config.GetValue<string>(BaseConstants.SelfUrlConfigSectionName));
+            
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .ConfigureKestrel(options => 
+                            options.ListenAnyIP(selfUri.Port))
+                        .UseStartup<Startup>();
+                });
+        }
     }
 }
